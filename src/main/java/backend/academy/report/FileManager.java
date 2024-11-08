f6c3f9e8af56e55b23612c23e1119306d6e411e3
DanilMartemyanov <martidanilka@gmail.com>
2024-11-08 20:42:35 +0300

    fix: fileNames, feat: add file in format md

--- src/main/java/backend/academy/report/FileManager.java
+++ src/main/java/backend/academy/report/FileManager.java
@@ -14,10 +14,10 @@ public class FileManager {
     public static List<String> getFileNamesInDirectory(String directoryPath) {
         try (Stream<Path> paths = Files.list(Paths.get(directoryPath))) {
             return paths
-                .filter(Files::isRegularFile) // Фильтруем только файлы (без директорий)
-                .map(Path::getFileName)       // Извлекаем имя файла
-                .map(Path::toString)          // Преобразуем в строку
-                .collect(Collectors.toList()); // Собираем в список
+                .filter(Files::isRegularFile)
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException("Error reading directory: " + directoryPath, e);
         }
